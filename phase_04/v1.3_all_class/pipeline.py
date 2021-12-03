dfb85024f46de85d245f704b837119698ff9c6d0
KumaranShivam5 <kumaranshivam57@gmail.com>
2021-12-03 14:16:17 +0530

    backup on 3 Dec , 2021

---
+++ phase_04/v1.3_all_class/pipeline.py
@@ -0,0 +1,84 @@
+import pandas as pd 
+import numpy as np 
+from matplotlib import pyplot as plt 
+from IPython.display import display
+import seaborn as sns
+
+def filter_data(data_sent , max_flux= -12):
+    data = data_sent.copy()
+    max_flux = -12
+    min_flux = 26
+    data = data[data['flux_aper']<max_flux]
+
+    data = data[data['significance']>2]
+    data_class = data[['class']]
+
+    data_sig = data['significance']
+    data_id = data['src_id']
+    data_name = data['src_n']
+    obs_info_params = [ 'livetime','likelihood','pileup_flag','mstr_sat_src_flag','mstr_streak_src_flag'   ,'gti_obs' , 'flux_significance_b'  , 'flux_significance_m' , 'flux_significance_s' , 'flux_significance_h' , 'flux_significance_u'    ]
+    data_val = data.drop(columns=obs_info_params)
+    return data_val
+
+def norm_data(data_sent):
+    data = data_sent.copy()
+    #data.replace()
+    for d in data:
+        max_val = np.amax(data[d])
+        min_val =  np.amin(data[d])
+        data[d] = (data[d]-min_val)/(max_val-min_val)
+    return data
+def std_data(data_sent):
+    data = data_sent.copy()
+    for d in data:
+        mean =  np.mean(data[d])
+        std = np.sqrt(np.var(data[d]))
+        data[d] = (data[d]-mean)/std 
+    return data
+def do_nothing(data_sent):
+    return data_sent
+
+def extract_data(data_sent , impute_fn = '',reduce_fn = ' ' , rf_impute=False):
+    data = data_sent.copy()
+    data = data.sample(frac=1)
+    data = filter_data(data)
+    #display(data)
+    data_id = data[[ 'class' ,'src_n' , 'src_id' ,'significance' , ]]
+    data_id = data_id
+    data_val = data.drop([ 'class' ,'src_n' , 'src_id' ,'significance' ,] , axis=1)
+    data_val = reduce_fn(data_val)
+    return data_val , data_id
+    #if(rf_impute):
+    #    data_val  , random_forest_imputer = impute_fn(data_val , data_id)
+    #else:
+    #    data_val = impute_fn(data_val)
+    data_val = reduce_fn(data_val)
+    data_val = data_val.reset_index(drop=True)
+    data_reduced = pd.concat([data_id , data_val] , axis=1)
+    if(rf_impute):
+        return(data_reduced , random_forest_imputer)
+    else:
+        return data_reduced
+
+
+import sklearn.neighbors._base
+from os import sys
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+
+
+from missingpy import MissForest 
+
+def rf_impute(d, i ):
+    data = pd.concat([i , d] , axis=1)
+    data = data.drop(columns=['src_n' , 'src_id' , 'significance' ,])
+    rf_imputer = MissForest()
+    #new_data = d.drop(columns= ['class'])
+    new_data = rf_imputer.fit_transform(d)
+    return new_data , rf_imputer
+
+
+def pipeline(train_set):
+    data_val , data_id   = extract_data(train_set ,  impute_fn= rf_impute , reduce_fn= do_nothing , rf_impute=True )
+
+    return data_val , data_id
+
